0313378b931e14183888261fbcdc135a4c553b46
Jurek <jurek2006@gmail.com>
2019-07-05 11:30:17 +0200

    27. Working on The Signup Form

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,10 +1,17 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
-import { MatButtonModule } from "@angular/material/button";
-import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
-const materialModules = [MatButtonModule, MatIconModule];
+const materialModules = [
+  MatButtonModule,
+  MatIconModule,
+  MatFormFieldModule,
+  MatInputModule
+];
 
 @NgModule({
   declarations: [],
