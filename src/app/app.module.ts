c0ac4b8fdc9bbb95c225b579dbd64beaf61fb236
Jurek <jurek2006@gmail.com>
2019-08-22 11:44:54 +0200

    84. Listening to Value Changes (of Firestore)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
 
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material.module';
@@ -39,7 +42,9 @@ import { StopTrainingComponent } from './training/current-training/stop-training
     FlexLayoutModule,
     FormsModule,
     ReactiveFormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBYrLMQmotCWgJJwyb_3H6mPt-p5_XTqF8',
+    authDomain: 'udemy-schwarz-fitness-tracker.firebaseapp.com',
+    databaseURL: 'https://udemy-schwarz-fitness-tracker.firebaseio.com',
+    projectId: 'udemy-schwarz-fitness-tracker',
+    storageBucket: 'udemy-schwarz-fitness-tracker.appspot.com',
+    messagingSenderId: '778600328774',
+    appId: '1:778600328774:web:45904c1d4d6b04ef'
+  }
 };
 
 /*
