88da5927cc8ef6a6d22399f5ae4ee334bc6d7994
Jurek <jurek2006@gmail.com>
2019-10-16 11:00:00 +0200

    107. Creating a SharedModule

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,21 +1,12 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MaterialModule } from '../material.module';
-import { FlexLayoutModule } from '@angular/flex-layout';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AngularFireAuthModule } from '@angular/fire/auth';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [SignupComponent, LoginComponent],
-  imports: [
-    CommonModule,
-    FormsModule,
-    ReactiveFormsModule,
-    MaterialModule,
-    FlexLayoutModule,
-    AngularFireAuthModule
-  ]
+  imports: [SharedModule, ReactiveFormsModule, AngularFireAuthModule]
 })
 export class AuthModule {}
--- src/app/training/training.module.ts
+++ src/app/training/training.module.ts
@@ -1,13 +1,10 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { TrainingComponent } from './training.component';
 import { CurrentTrainingComponent } from './current-training/current-training.component';
 import { NewTrainingComponent } from './new-training/new-training.component';
 import { PastTrainingsComponent } from './past-trainings/past-trainings.component';
 import { StopTrainingComponent } from './current-training/stop-training-component';
-import { FormsModule } from '@angular/forms';
-import { MaterialModule } from '../material.module';
-import { FlexLayoutModule } from '@angular/flex-layout';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -17,7 +14,7 @@ import { FlexLayoutModule } from '@angular/flex-layout';
     PastTrainingsComponent,
     StopTrainingComponent
   ],
-  imports: [CommonModule, FormsModule, MaterialModule, FlexLayoutModule],
+  imports: [SharedModule],
   entryComponents: [StopTrainingComponent]
 })
 export class TrainingModule {}
