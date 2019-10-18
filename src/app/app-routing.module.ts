e66ed173580e0e9e4a07ce29fa0d56fb19fe8605
Jurek <jurek2006@gmail.com>
2019-10-18 07:05:03 +0200

    110. Moving the Auth Guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { AuthGuard } from './auth/auth.guard';
 // import { TrainingModule } from './training/training.module';
 
 const routes: Routes = [
@@ -8,7 +9,8 @@ const routes: Routes = [
   {
     path: 'training',
     // loadChildren: () => TrainingModule
-    loadChildren: './training/training.module#TrainingModule'
+    loadChildren: './training/training.module#TrainingModule',
+    canLoad: [AuthGuard]
   }
 ];
 
